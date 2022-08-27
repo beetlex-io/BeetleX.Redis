c3a37125a30323e93c7f0d75b5242c985d75f1f0
DESKTOP-2N3GSM4\henry <henryfan@msn.com>
2022-08-27 10:12:12 +0800

    fix issues/43

--- src/RedisHost.cs
+++ src/RedisHost.cs
@@ -113,9 +113,8 @@ namespace BeetleX.Redis
 
                     Commands.SELECT select = new Commands.SELECT(DB);
                     var req = new RedisRequest(null, client, select, typeof(string));
-                    var t = req.Execute(db);
-                    t.Wait();
-                    return t.Result;
+                    return await req.Execute(db);
+
                 }
                 else
                 {
@@ -123,6 +122,7 @@ namespace BeetleX.Redis
                     return new Result { ResultType = ResultType.NetError, Messge = client.TcpClient.LastError.Message };
                 }
             }
+            this.Available = true;
             return new Result { ResultType = ResultType.Simple, Messge = "Connected" };
         }
 
